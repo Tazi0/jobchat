c224aa868871d723ad645e395c3afbd8dfb4da41
Tazio <tazio2003@gmail.com>
2019-01-02 17:22:28 +0100

    Update V2
    Now has the command /rp to activate

--- jobchat/main.lua
+++ jobchat/main.lua
@@ -3,26 +3,23 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-AddEventHandler('chatMessage', function(Source, Name, Msg)
-    args = removenormal(Msg, " ")
-    CancelEvent()
-    if string.find(args[1], "/") then
-        local cmd = args[1]
-        table.remove(args, 1)
-    else     
-		local xPlayer = ESX.GetPlayerFromId(Source)
-		str = xPlayer.job.name
-		str = str:gsub("^%l", string.upper)
-        TriggerClientEvent('chatMessage', -1, str .. " ׀ " .. Name, { 255, 0, 0 }, Msg)           
-    end
+AddEventHandler('chatMessage', function(source, name, msg)
+	sm = stringsplit(msg, " ");
+	if sm[1] == "/rp" then
+		CancelEvent()
+    xPlayer = ESX.GetPlayerFromId(source)
+    str = xPlayer.job.name
+    job = str:gsub("^%l", string.upper)
+    TriggerClientEvent('chatMessage', -1, job .. " ׀ " .. name, { 255, 0, 0 }, string.sub(msg,4))
+	end
 end)
 
-function removenormal(input, sep)
+function stringsplit(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
     local t={} ; i=1
-    for str in string.gmatch(input, "([^"..sep.."]+)") do
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
         t[i] = str
         i = i + 1
     end
